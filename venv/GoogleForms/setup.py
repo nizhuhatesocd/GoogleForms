dfff5e5fb38a65bac784025bd8403b7afdb41712
root <ashkanebtekari@gmail.com>
2020-12-02 05:15:27 -0500

    setup file problem solved

--- venv/GoogleForms/setup.py
+++ venv/GoogleForms/setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
         url="GoogleForms GitHub Url",
         packages=setuptools.find_packages(),
         classifiers=[
-            "Programming :: Language :: Python3",
+            "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
